fcc87cb230257ba35eb541ac8af54eb9645e1622
Luna <dlramos194@gmail.com>
2020-11-30 21:36:49 -0600

    fixed col typo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,20 @@
 import React from 'react';
+// import {Linking} from 'react-native'
 import "./style.css"
 
 function Footer(){
-
+  
     return(
- <div className ="footer">
-   
-     <p>Linked In Logo  |  Github Logo</p>
-
-  
-  
- </div>
-
+        <div className ="footer">
+            <a target="blank" href="https://github.com/DianaLRS">Github </a>  
+            <a target="blank" href="https://www.linkedin.com/in/diana-l-ramos-7b06241a9/">LinkedIn</a>
+         
+         
+         
+        </div>
     )
-
-
 }
+
+
 
 export default Footer;
