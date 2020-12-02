8da818d86bb8e3d71650f631738ba7ca2fd92741
Luna <dlramos194@gmail.com>
2020-12-01 22:00:40 -0600

    deleting unused files

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,9 +1,8 @@
 import React from "react";
 import Cards from "../components/Cards/Cards";
 import Container from "../components/Container/Conatiner";
-import "./Portfolio.css"
-// import Col from "../components/Grid/Col"
-// import Row from "../components/Grid/Row"
+
+
 
 function Portfolio(){
 
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Mainstyle.css";
 import Conatiner from "../components/Container/Conatiner";
-import HomeBtn from "../components/Buttons/HomeBtn"
+
 function Main(){
 
     return(
@@ -13,7 +13,7 @@ function Main(){
                 <h2 className ="name">My name is Diana</h2>
                 <p className="intro"> I'm a full-stack developer with a preference for clean, minimalistic, and user-friendly websites</p>
 
-                {/* <HomeBtn/> */}
+          
                 </div>
 
             </Conatiner>
