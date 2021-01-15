53ede444ce2f8d1114cb284b51ec16f0d27f92b9
Luna <dlramos194@gmail.com>
2021-01-14 18:13:28 -0600

    added server side

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+app.use(routes);
+
+// Send every other request to the React app
+// Define any API routes before this runs
+// Connect to the Mongo DB
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/Portfoliodb", 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() =>{
+  console.log("it worked")
+  
+}).catch((e) => {
+  console.log("it didnt work")
+  console.log(e)
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose has connected!");
+})
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/public/index.html"));
+// });
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
